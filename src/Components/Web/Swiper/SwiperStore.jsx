12e02972235253befabe05365659de432140355f
Hadeel-Abdeljaleel <h.02.adeel@gmail.com>
2024-05-21 19:47:31 +0300

    update swipper final

--- src/Components/Web/Swiper/SwiperStore.jsx
+++ src/Components/Web/Swiper/SwiperStore.jsx
@@ -19,7 +19,6 @@ export default function SwiperStoare() {
         navigation
         loop={true}
         autoplay={{ delay: 5000 }}
-        pagination={{ clickable: true }}
         className=' d-flex align-content-center header  h-100 w-100'
 
       >
@@ -31,6 +30,27 @@ export default function SwiperStoare() {
         </span>        </SwiperSlide>
         
         <SwiperSlide className=' h-100 w-100'><img className='h-100 w-100' src="/images/contact-us.jpg" alt="swiper-slide" />
+        <span className='swiper-text'>
+          <h1 className='swiper-text2'>استكشف فريقنا ورؤيتنا</h1>
+          <Link to="/about">        <button className='btn border border-white bg-transparent text-white rounded-5 fw-bold store-header-botton mt-2'>شاهد الآن</button>
+          </Link>
+        </span>
+        </SwiperSlide>
+        <SwiperSlide className=' h-100 w-100'><img className='h-100 w-100' src="/images/x3.jpeg" alt="swiper-slide" />
+        <span className='swiper-text'>
+          <h1 className='swiper-text2'>كل ما تحتاجه لرحلتك الأمثل</h1>
+          <Link to="/products">        <button className='btn border border-white bg-transparent text-white rounded-5 fw-bold store-header-botton mt-2'>تسوق الآن</button>
+          </Link>
+        </span>
+        </SwiperSlide>
+        <SwiperSlide className=' h-100 w-100'><img className='h-100 w-100' src="/images/x5.jpeg" alt="swiper-slide" />
+        <span className='swiper-text'>
+          <h1 className='swiper-text2 '> استعد لاستكشاف الطرقات الجديدة واكتشاف المناظر الخلابة على عجلتك</h1>
+          <Link to="/trips">        <button className='btn border border-white bg-transparent text-white rounded-5 fw-bold store-header-botton mt-2'>شارك معنا</button>
+          </Link>
+        </span>        </SwiperSlide>
+        
+        <SwiperSlide className=' h-100 w-100'><img className='h-100 w-100' src="/images/x6.jpeg" alt="swiper-slide" />
         <span className='swiper-text'>
           <h1 className='swiper-text2'>استكشف فريقنا ورؤيتنا</h1>
           <Link to="/about">        <button className='btn border border-white bg-transparent text-white rounded-5 fw-bold store-header-botton mt-2'>شاهد الآن</button>
